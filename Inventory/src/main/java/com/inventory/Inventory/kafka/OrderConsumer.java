dec7069f1254d9c89b5f3c80c0be20a846e7744d
Pansilu Ruvinsara <ruvinsara23@gmail.com>
2024-11-28 14:14:19 +0530

    create consumer in inventory

--- Inventory/src/main/java/com/inventory/Inventory/kafka/OrderConsumer.java
+++ Inventory/src/main/java/com/inventory/Inventory/kafka/OrderConsumer.java
@@ -1,6 +1,21 @@
 package com.inventory.Inventory.kafka;
 
+import com.base.base.dto.OrderEventDTO;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderConsumer {
 
+private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+@KafkaListener(
+        topics = "${spring.kafka.template.default-topic}",
+        groupId = "${spring.kafka.consumer.group-id}"
+)
+    public void consumer(OrderEventDTO orderEventDTO) {
+    LOGGER.info("consume orderEventDTO: {}",orderEventDTO.toString());
+}
 
 }
